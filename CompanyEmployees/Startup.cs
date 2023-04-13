55cc6245f194b48310a1b3f96af228622bc20224
CyberS1337 <95416232+CyberS1337@users.noreply.github.com>
2023-04-13 21:53:11 +0300

    Выполнить 8 лабораторную работу.

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.HttpOverrides;
-using CompanyEmployees.Extensions;
+﻿using Contracts;
+using LR_WEB_API.ActionFilters;
+using LR_WEB_API.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NLog;
-using AutoMapper;
-using Entities.Models;
-using Entities.DataTransferObjects;
+//using ShopApi.Extensions;
 
-namespace CompanyEmployees;
+namespace ShopApi;
 
 public class Startup
 {
@@ -21,22 +23,40 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddScoped<ValidateWarehouseExistsAttribute>();
+        services.AddScoped<ValidateEmployeeForCompanyExistsAttribute>();
+        services.AddScoped<ValidateCompanyExistsAttribute>();
+        services.AddScoped<ValidationFilterAttribute>();
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
         services.ConfigureCors();
         services.ConfigureIISIntegration();
         services.ConfigureLoggerService();
         services.ConfigureSqlContext(Configuration);
         services.ConfigureRepositoryManager();
+        services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
-        services.AddControllers(config =>   
-        { 
+        services.AddAutoMapper(typeof(Startup));
+        services.AddControllers(config =>
+        {
             config.RespectBrowserAcceptHeader = true;
             config.ReturnHttpNotAcceptable = true;
-        }).AddXmlDataContractSerializerFormatters().AddCustomCSVFormatter();
+
+        })
+
+        .AddXmlDataContractSerializerFormatters()
+        .AddCustomCSVFormatter()
+        .AddNewtonsoftJson();
+
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+    ILoggerManager logger)
     {
         if (env.IsDevelopment())
         {
@@ -44,11 +64,23 @@ public class Startup
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
+        app.ConfigureExceptionHandler(logger);
         app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseCors("CorsPolicy");
         app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.All
+        });
+
+        app.UseHsts();
+
+        app.UseStaticFiles();
+
+        app.UseCors("CorsPolicy");
+
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+
         {
             ForwardedHeaders = ForwardedHeaders.All
         });
@@ -61,20 +93,5 @@ public class Startup
         {
             endpoints.MapControllers();
         });
-    }
-
-    public class MappingProfile : Profile
-    {
-        public MappingProfile()
-        {
-            CreateMap<Company, CompanyDto>().ForMember(c => c.FullAddress, opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
-            CreateMap<Animal, AnimalDto>();
-            CreateMap<Product, ProductDto>();
-            CreateMap<Employee, EmployeeDto>();
-            CreateMap<CompanyForCreationDto, Company>();
-            CreateMap<AnimalForCreationDto, Animal>();
-            CreateMap<EmployeeForCreationDto, Employee>();
-            CreateMap<ProductForCreationDto, Product>();
-        }
     }
 }
